33dbb1fda9cf51cd74c2b90a90cc3a36a8e544f1
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-05-04 14:30:42 +0300

    setup tailwindcss

--- server/src/index.ts
+++ server/src/index.ts
@@ -15,14 +15,13 @@ const connect_to_db = async () => {
     console.error('Error connecting to MongoDB:', error);
   }
 };
+connect_to_db();
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-
-connect_to_db();
 
 app.listen(3001, () => {
   console.log('Server is running on port 3001');
