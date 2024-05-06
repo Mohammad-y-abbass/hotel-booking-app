5aa36bb83125a4dc45b30eb5b9bc23cc25679eed
Mohammad Abbass <mhmd.abbass.90@gmail.com>
2024-05-06 12:15:28 +0300

    conditional render header link

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,19 +1,30 @@
 import { Link } from 'react-router-dom';
+import { useAppContext } from '../contexts/AppContext';
 
 const Header = () => {
+  const { isLoggedIn } = useAppContext();
+
   return (
-    <div className='bg-blue-800 py-6'>
-      <div className='container mx-auto flex justify-between items-center'>
-        <span className='text-white text-3xl font-bold tracking-tight'>
+    <div className='py-6 bg-blue-800'>
+      <div className='container flex items-center justify-between mx-auto'>
+        <span className='text-3xl font-bold tracking-tight text-white'>
           <Link to='/'>Booking App</Link>
         </span>
         <span className='flex space-x-2'>
-          <Link
-            to='/sign-in'
-            className='flex bg-white items-center text-blue-600 px-3 font-bold hover:bg-gray-100'
-          >
-            Sign In
-          </Link>
+          {isLoggedIn ? (
+            <>
+              <Link to='/my-bookings'>My Bookings</Link>
+              <Link to='/my-hotels'>My Hotels</Link>
+              <button>Sign out</button>
+            </>
+          ) : (
+            <Link
+              to='/sign-in'
+              className='flex items-center px-3 font-bold text-blue-600 bg-white hover:bg-gray-100'
+            >
+              Sign In
+            </Link>
+          )}
         </span>
       </div>
     </div>
